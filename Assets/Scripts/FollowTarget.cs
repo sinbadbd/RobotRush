b9c2bcdb4bf19e40b00cae343185405d8e60285c
imran <imufun2@gmail.com>
2021-02-11 12:58:47 +0600

    Camera Setup

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform target;
+
+    Vector3 velocity = Vector3.zero;
+
+    public float smoothTime = .15f;
+
+    private void FixedUpdate()
+    {
+        Vector3 targetPos = target.position;
+
+        targetPos.z = transform.position.z;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+
+    }
+}
