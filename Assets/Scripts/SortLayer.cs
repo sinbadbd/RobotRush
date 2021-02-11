d0ae80d33d2626d5d7e0a567191eda1d8e997df7
imran <imufun2@gmail.com>
2021-02-11 14:26:19 +0600

    layer sorting

---
+++ Assets/Scripts/SortLayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortLayer : MonoBehaviour
+{
+    public string sortLayerName;
+    public GameObject[] Go;
+  
+
+    private void Start()
+    {
+        for (int i = 0; i < Go.Length; i++)
+        {
+            Go[i].GetComponent<Renderer>().sortingLayerName = sortLayerName;
+        }
+    }
+}
